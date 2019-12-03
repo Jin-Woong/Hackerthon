a23788783e548b93d859b9e0f291036c65a97912
Jin-Woong <kjw7236@gmail.com>
2019-12-03 10:18:02 +0900

    Add check ngrok + set webhook

---
+++ telegram-django/bus/check_ngrok_webhook.py
@@ -0,0 +1,19 @@
+import os
+import json
+import requests
+from decouple import config
+
+# def get_ngrok_url():
+url = "http://localhost:4040/api/tunnels/"
+res = requests.get(url)
+res_unicode = res.content.decode("utf-8")
+res_json = json.loads(res_unicode)
+ngrok_url = ''
+for i in res_json["tunnels"]:
+    if i['name'] == 'command_line':
+        print(i['public_url'])
+        ngrok_url = i['public_url']
+
+token = config('TOKEN')
+api_url = f'https://api.telegram.org/bot{token}//setWebhook?url={ngrok_url}/{token}'
+os.system('curl '+api_url)
