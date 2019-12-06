749d252178f650825c165d5100b51802bb9a1814
Jin-Woong <kjw7236@gmail.com>
2019-12-06 09:35:02 +0900

    Modify crontab 1min to 30sec

--- telegram-django/bus/views.py
+++ telegram-django/bus/views.py
@@ -374,7 +374,7 @@ def tel(request):
                     # OS마다 다르다.. ubuntu의 ubuntu는 1000, amazon linux의 ec2-user는 500
                     user = f'sudo useradd -d /home/ubuntu -u 1000 -o {chat_id}'  # ubuntu 와 같은 uid 를 갖도록 계정 생성
                     # 크론탭 시간 1분은 좀 긴거 같고 30초 간격으로 수정해야할듯..
-                    cron = f'(crontab -l 2>/dev/null; echo "*/1 * * * * python3 /home/ubuntu/telegram_alarm/telegram-django/bus_alarm.py {chat_id} {minute[0]} go") | sudo crontab -u {chat_id} -'
+                    cron = f'(crontab -l 2>/dev/null; echo "*/1 * * * * python3 /home/ubuntu/telegram_alarm/telegram-django/bus_alarm.py {chat_id} {minute[0]} go & sleep 30; python3 /home/ubuntu/telegram_alarm/telegram-django/bus_alarm.py {chat_id} {minute[0]} go") | sudo crontab -u {chat_id} -'
                     os.system(user)
                     os.system(cron)
                     msg = f'{busgo.go_bus_number}번 버스 도착 {minute[0]}분 전 알림\n' \
